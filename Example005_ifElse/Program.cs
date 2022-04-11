c6e304af4db87a920d8544233b7c6aceeaef9ef2
Zhadko_Aleksey <Arxangel1310@mail.ru>
2022-04-11 13:52:50 +0300

    ifElse

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if(username.ToLower == "masha")
+{
+    Console.Write("Good, hello Masha");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
